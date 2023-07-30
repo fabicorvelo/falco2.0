66c56fa45dff1653f91f1a1dac20fec3d9bc48b2
fabicorvelo <30451431+fabicorvelo@users.noreply.github.com>
2023-07-30 00:06:12 -0300

    Add files via upload

---
+++ about.js
@@ -0,0 +1,42 @@
+
+function changeImageOver1()
+{
+    document.getElementById("img_responsive").src ="icons7-verde.png";
+}
+
+function changeImageLeave1()
+{
+    document.getElementById("img_responsive").src ="icons7.png";
+}
+
+
+
+function changeImageOver()
+{
+    document.getElementById("img_heart").src ="icons8-verde.png";
+}
+
+function changeImageLeave()
+{
+    document.getElementById("img_heart").src ="icons8.png";
+}
+
+function changeImageOver2()
+{
+    document.getElementById("img_design").src ="icons9-verde.png";
+}
+
+function changeImageLeave2()
+{
+    document.getElementById("img_design").src ="icons9.png";
+}
+
+function changeImageOver3()
+{
+    document.getElementById("img_suporte").src ="icons10-verde.png";
+}
+
+function changeImageLeave3()
+{
+    document.getElementById("img_suporte").src ="icons10.png";
+}
